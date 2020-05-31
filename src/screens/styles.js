23c1ddfcde5e8ffce00970fafe4aff2ac1d46576
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-05-30 22:21:58 -0300

    feta: screen home

---
+++ src/screens/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+import { ifIphoneX, getStatusBarHeight } from 'react-native-iphone-x-helper';
+import bg from '../assets/images/login.jpg';
+
+export const Container = styled.ImageBackground.attrs({
+  source: bg,
+  imageStyle: {
+    opacity: 0.3,
+  },
+})`
+  flex: 1;
+  padding: 30px;
+  justify-content: space-evenly;
+  padding-top: ${ifIphoneX ? getStatusBarHeight(true) : 0};
+`;
