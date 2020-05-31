cc99dd91906c14e52f1730e160c39f7c932d6cb0
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-05-31 12:54:14 -0300

    feat: added animations

---
+++ src/assets/animations/index.js
@@ -0,0 +1,13 @@
+import setting from './setting.json';
+import hug from './hug.json';
+import search from './searching.json';
+import connect from './connect.json';
+
+const animations = {
+  setting,
+  hug,
+  search,
+  connect,
+};
+
+export { animations };
