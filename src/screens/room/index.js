54ed740f6039c5ba1a6efbe7c568ebfcd3b3368f
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-05-31 14:52:51 -0300

    feat: screen home

--- src/screens/room/index.js
+++ src/screens/room/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { Container } from './styles';
+import YouTube from 'react-native-youtube';
 
-export default function Home() {
-  return <Container></Container>;
+export default function ({ route, navigation }) {
+  const { idVideo } = route.params;
+  return (
+    <YouTube
+      apiKey=""
+      videoId={idVideo} // The YouTube video ID
+      play // control playback of video with true/false
+      fullscreen // control whether the video should play in fullscreen or inline
+      loop // control whether the video should loop when ended
+      style={{ alignSelf: 'stretch', height: 500 }}
+    />
+  );
 }
